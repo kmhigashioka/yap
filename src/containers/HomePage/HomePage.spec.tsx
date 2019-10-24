63e867e5b617ecd7cde21c6cb054074aaf4c1376
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-10-24 21:59:56 +0800

    Update test

--- src/containers/HomePage/HomePage.spec.tsx
+++ src/containers/HomePage/HomePage.spec.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import HomePage from './HomePage';
 
 jest.mock('./AppBar', () => (): string => 'MockAppBar');
+jest.mock('../ExpensesPage/Loadable', () => (): string => 'MockExpensesPage');
 
 describe('<HomePage />', () => {
   it('should render <AppBar />', () => {
-    const { getByText } = render(<HomePage />);
+    const { getByText } = render(<Component initialEntries={['/']} />);
 
     expect(getByText('MockAppBar')).toBeInTheDocument();
   });
+
+  it('should render <ExpensesPage />', () => {
+    const { getByText } = render(<Component initialEntries={['/']} />);
+
+    expect(getByText('MockExpensesPage')).toBeInTheDocument();
+  });
 });
+
+type ComponentProps = {
+  initialEntries: string[] | undefined;
+};
+
+function Component({ initialEntries }: ComponentProps): React.ReactElement {
+  return (
+    <MemoryRouter initialEntries={initialEntries}>
+      <HomePage />
+    </MemoryRouter>
+  );
+}
