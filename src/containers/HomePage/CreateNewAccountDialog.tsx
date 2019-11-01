6cabf2289489a1c67c999f7338b8160e6d76a56a
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-11-01 10:14:53 +0800

    Create new account (#8)
    * Exclude jsx-props-no-spreading for TextField and html
    
    * Implement Create new account
    
    * Update test

---
+++ src/containers/HomePage/CreateNewAccountDialog.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { useFormState } from 'react-use-form-state';
+import { IFormDialogProps } from './types';
+
+const CreateNewAccountDialog: React.FC<IFormDialogProps> = ({
+  open,
+  onClose,
+  addAccount,
+}) => {
+  const [formState, { text, number }] = useFormState({
+    name: '',
+    abbreviation: '',
+    startingBalance: 0,
+  });
+
+  const handleSubmit = (evt: React.FormEvent<EventTarget>): void => {
+    evt.preventDefault();
+
+    const { name, abbreviation, startingBalance } = formState.values;
+    addAccount({
+      id: new Date().getUTCMilliseconds(),
+      name,
+      abbreviation,
+      balance: startingBalance,
+    });
+    formState.clear();
+    onClose();
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <form onSubmit={handleSubmit}>
+        <DialogTitle>Create New Account</DialogTitle>
+        <DialogContent>
+          <TextField
+            {...text('name')}
+            autoFocus
+            margin="dense"
+            label="Name"
+            required
+            fullWidth
+          />
+          <TextField
+            {...text('abbreviation')}
+            margin="dense"
+            label="Abbreviation (Maximum of 3 characters)"
+            inputProps={{ maxLength: 3 }}
+            required
+            fullWidth
+          />
+          <TextField
+            {...number('startingBalance')}
+            margin="dense"
+            label="Starting Balance"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose} color="primary">
+            Cancel
+          </Button>
+          <Button type="submit" color="primary">
+            Create
+          </Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+};
+
+export default CreateNewAccountDialog;
--- src/containers/HomePage/types.ts
+++ src/containers/HomePage/types.ts
@@ -1,11 +1,21 @@
 export type THomePageContext = {};
+
 export interface IAppBarProps {
   accounts: Account[];
   activeAccount: Account | null;
   setActiveAccount: (account: Account | null) => void;
+  addAccount: (account: Account) => void;
 }
+
+export interface IFormDialogProps {
+  open: boolean;
+  onClose: () => void;
+  addAccount: (account: Account) => void;
+}
+
 export type Account = {
   id: number;
   name: string;
   abbreviation: string;
+  balance: number;
 };
