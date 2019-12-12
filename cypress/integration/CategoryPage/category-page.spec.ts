eeaed6af494fa1d6ba0f9cc302aa57835dac5a51
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-12-12 10:29:43 +0800

    Update test

--- cypress/integration/CategoryPage/category-page.spec.ts
+++ cypress/integration/CategoryPage/category-page.spec.ts
@@ -1,9 +1,29 @@
 describe('Category Page', () => {
+  let polyfill;
+
+  before(() => {
+    const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
+    cy.request(polyfillUrl).then(response => {
+      polyfill = response.body;
+    });
+  });
+
   beforeEach(() => {
-    cy.visit('/category');
+    cy.server();
+    cy.route('/api/accounts', 'fixture:accounts.json');
+    cy.route('/api/usercategories?userId=1', 'fixture:usercategories.json');
+    cy.visit('/category', {
+      onBeforeLoad(win) {
+        const winCopy = win;
+        delete winCopy.fetch;
+        winCopy.eval(polyfill);
+        winCopy.fetch = win.unfetch;
+      },
+    });
   });
 
   it("should set the Charges' display to off", () => {
+    cy.route('PATCH', '/api/usercategories/1', {});
     cy.findByLabelText('Charges').click();
   });
 
