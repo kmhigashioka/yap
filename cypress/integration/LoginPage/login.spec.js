dd157350da85cabeab67a8bd1287df87b0688f9c
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-01-07 10:07:53 +0800

    Use real auth server

--- cypress/integration/LoginPage/login.spec.js
+++ cypress/integration/LoginPage/login.spec.js
@@ -2,8 +2,10 @@ describe('LoginPage', () => {
   it('should login', () => {
     cy.visit('/login');
 
-    cy.findByPlaceholderText('Username').type('myfancyusername');
-    cy.findByPlaceholderText('Password').type('password');
+    cy.findByPlaceholderText('Username').type('m2m');
+    cy.findByPlaceholderText('Password').type('secret');
     cy.findByText('Login').click();
+
+    cy.title().should('contain', 'Dashboard');
   });
 });
