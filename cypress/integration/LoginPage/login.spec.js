150d5d2f87bdc3bcab8e04e806ade8831835796d
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-01-07 10:18:34 +0800

    Add test

--- cypress/integration/LoginPage/login.spec.js
+++ cypress/integration/LoginPage/login.spec.js
@@ -1,5 +1,5 @@
 describe('LoginPage', () => {
-  it('should login', () => {
+  it('should successfully login', () => {
     cy.visit('/login');
 
     cy.findByPlaceholderText('Username').type('m2m');
@@ -7,5 +7,17 @@ describe('LoginPage', () => {
     cy.findByText('Login').click();
 
     cy.title().should('contain', 'Dashboard');
+  });
+
+  it('should display error message when using invalid credential', () => {
+    cy.visit('/login');
+
+    cy.findByPlaceholderText('Username').type('invalid');
+    cy.findByPlaceholderText('Password').type('credential');
+    cy.findByText('Login').click();
+
+    cy.findByText('Invalid username or password.').should('be.visible');
+    cy.findByPlaceholderText('Username').click();
+    cy.findByText('Invalid username or password.').should('not.be.visible');
   });
 });
