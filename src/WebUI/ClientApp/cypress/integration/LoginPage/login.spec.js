f3fbe76b0e496bac9a0b8572a9e185dd9289db53
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-01-21 23:48:25 +0800

    Update test

--- src/WebUI/ClientApp/cypress/integration/LoginPage/login.spec.js
+++ src/WebUI/ClientApp/cypress/integration/LoginPage/login.spec.js
@@ -1,6 +1,27 @@
 describe('LoginPage', () => {
+  let polyfill;
+
+  before(() => {
+    const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
+    cy.request(polyfillUrl).then(response => {
+      polyfill = response.body;
+    });
+  });
+
   it('should successfully login', () => {
-    cy.visit('/login');
+    cy.server();
+    cy.route('POST', '/connect/token', {
+      access_token: 'access_token',
+      refresh_token: 'refresh_token',
+    });
+    cy.visit('/login', {
+      onBeforeLoad(win) {
+        const winCopy = win;
+        delete winCopy.fetch;
+        winCopy.eval(polyfill);
+        winCopy.fetch = win.unfetch;
+      },
+    });
 
     cy.findByPlaceholderText('Username').type('m2m');
     cy.findByPlaceholderText('Password').type('secret');
@@ -10,7 +31,21 @@ describe('LoginPage', () => {
   });
 
   it('should display error message when using invalid credential', () => {
-    cy.visit('/login');
+    cy.server();
+    cy.route({
+      url: '/connect/token',
+      method: 'POST',
+      status: 400,
+      response: {},
+    });
+    cy.visit('/login', {
+      onBeforeLoad(win) {
+        const winCopy = win;
+        delete winCopy.fetch;
+        winCopy.eval(polyfill);
+        winCopy.fetch = win.unfetch;
+      },
+    });
 
     cy.findByPlaceholderText('Username').type('invalid');
     cy.findByPlaceholderText('Password').type('credential');
