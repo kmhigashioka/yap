52085846d1da915466523613caeaf39914c671a4
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-03-01 12:02:19 +0800

    Add /api/users/me endpoint

---
+++ src/Application/Users/Queries/GetMeQuery.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Dtos;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Omu.ValueInjecter;
+
+namespace Application.Users.Queries
+{
+    public class GetMeQuery: IRequest<ApplicationUserDto>
+    {
+        public string CurrentUserId { get; set; }
+    }
+
+    public class GetMeQueryHandler: IRequestHandler<GetMeQuery, ApplicationUserDto>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public GetMeQueryHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<ApplicationUserDto> Handle(GetMeQuery request, CancellationToken cancellationToken)
+        {
+            var appuser = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == request.CurrentUserId, cancellationToken);
+            return Mapper.Map<ApplicationUserDto>(appuser);
+        }
+    }
+}
--- src/WebUI/Controllers/UsersController.cs
+++ src/WebUI/Controllers/UsersController.cs
@@ -3,6 +3,9 @@ using Application.Users.Commands;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Users.Queries;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebUI.Controllers
 {
@@ -10,6 +13,13 @@ namespace WebUI.Controllers
     [Produces("application/json")]
     public class UsersController: ApiController
     {
+        private readonly ICurrentUserService _currentUserService;
+
+        public UsersController(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
         [HttpPost]
         [ProducesResponseType(typeof(ApplicationUserDto), StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -17,6 +27,17 @@ namespace WebUI.Controllers
         {
             var result = await Mediator.Send(request);
             return Created("GetUser", result);
+        }
+
+        [Authorize]
+        [HttpGet("me")]
+        public async Task<IActionResult> Me()
+        {
+            var response = await Mediator.Send(new GetMeQuery
+            {
+                CurrentUserId = _currentUserService.UserId
+            });
+            return Ok(response);
         }
     }
 }
