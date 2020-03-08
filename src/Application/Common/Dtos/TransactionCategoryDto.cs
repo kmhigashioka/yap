fb78d90e1a4dd6bee635d161b1266036f3490e2f
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-03-09 00:17:21 +0800

    Add /api/TransactionCategories

---
+++ src/Application/Common/Dtos/TransactionCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common.Dtos
+{
+    public class TransactionCategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Display { get; set; }
+    }
+}
