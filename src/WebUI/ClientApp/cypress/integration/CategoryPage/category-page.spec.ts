61df582da578c6100b2d9936870157e27e8800b6
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-03-09 22:44:44 +0800

    Update test

--- src/WebUI/ClientApp/cypress/integration/CategoryPage/category-page.spec.ts
+++ src/WebUI/ClientApp/cypress/integration/CategoryPage/category-page.spec.ts
@@ -1,15 +1,9 @@
 describe('Category Page', () => {
   beforeEach(() => {
     cy.server();
-    cy.route('/api/accounts', 'fixture:accounts.json');
-    cy.route('/api/usercategories?userId=1', 'fixture:usercategories.json');
+    cy.route('/api/TransactionCategories', 'fixture:usercategories.json');
     cy.login();
     cy.visit('/category');
-  });
-
-  it("should set the Charges' display to off", () => {
-    cy.route('PATCH', '/api/usercategories/1', {});
-    cy.findByLabelText('Charges').click();
   });
 
   it('should filter Category by W', () => {
