86ed700353b17393b3d289d54fac29e19a5a1a1b
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-03-26 09:09:07 +0800

    Add endpoint for getting transactions

---
+++ src/Application/Common/Dtos/TransactionDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Domain.Entities;
+using Domain.Enums;
+
+namespace Application.Common.Dtos
+{
+    public class TransactionDto
+    {
+
+        public int Id { get; set; }
+        public TransactionType Type { get; set; }
+        public TransactionCategoryDto Category { get; set; }
+        public float Amount { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
