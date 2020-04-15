ab48157068969d5c6cc4663d4be33a18889ff0e2
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-04-15 23:14:04 +0800

    Use implicit declaration

--- tests/Application.Tests/Todos/Queries/GetTodosQueryTests.cs
+++ tests/Application.Tests/Todos/Queries/GetTodosQueryTests.cs
@@ -60,13 +60,12 @@ namespace Application.Tests.Todos.Queries
 
         private Mock<DbSet<T>> MockDbSet<T>(IEnumerable<T> list) where T : class, new()
         {
-            IQueryable<T> queryableList = list.AsQueryable();
-            Mock<DbSet<T>> dbSetMock = new Mock<DbSet<T>>();
+            var queryableList = list.AsQueryable();
+            var dbSetMock = new Mock<DbSet<T>>();
             dbSetMock.As<IQueryable<T>>().Setup(x => x.Provider).Returns(queryableList.Provider);
             dbSetMock.As<IQueryable<T>>().Setup(x => x.Expression).Returns(queryableList.Expression);
             dbSetMock.As<IQueryable<T>>().Setup(x => x.ElementType).Returns(queryableList.ElementType);
             dbSetMock.As<IQueryable<T>>().Setup(x => x.GetEnumerator()).Returns(() => queryableList.GetEnumerator());
-            //dbSetMock.Setup(x => x.Create()).Returns(new T());
 
             return dbSetMock;
         }
