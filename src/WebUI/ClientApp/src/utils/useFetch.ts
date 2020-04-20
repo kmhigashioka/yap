2be31a1da580d85757f5b38090451e9f409b79aa
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-04-20 22:28:33 +0800

    Add support for base url (#138)

--- src/WebUI/ClientApp/src/utils/useFetch.ts
+++ src/WebUI/ClientApp/src/utils/useFetch.ts
@@ -12,8 +12,12 @@ export default (): UseFetch => {
   return {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     requestWithToken: async <T>(url: string, options?: any): Promise<T> => {
+      const requestUrl =
+        url.indexOf('http') === 0
+          ? url
+          : `${process.env.REACT_APP_API_URL || ''}${url}`;
       try {
-        const data = await requestWithToken<T>(url, options);
+        const data = await requestWithToken<T>(requestUrl, options);
         return data;
       } catch (error) {
         if (error.message === 'Expired session.') {
