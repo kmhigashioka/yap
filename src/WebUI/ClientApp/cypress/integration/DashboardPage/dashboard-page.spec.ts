e0f75982b4dc07942a693c93b78276ec898e5859
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-10-28 00:25:32 +0800

    Add more coverage

--- src/WebUI/ClientApp/cypress/integration/DashboardPage/dashboard-page.spec.ts
+++ src/WebUI/ClientApp/cypress/integration/DashboardPage/dashboard-page.spec.ts
@@ -17,4 +17,30 @@ describe('Dashboard', () => {
     cy.findByText('Account successfully deleted.').should('be.visible');
     cy.findByTestId(testid).should('not.be.visible');
   });
+
+  it('should able to handle delete account error API', () => {
+    cy.route({
+      method: 'DELETE',
+      url: '/api/Users/Accounts?accountId=*',
+      status: 400,
+      response: {
+        message: 'Error message from API',
+      },
+      delay: 1500,
+    });
+    const testid = 'account-1';
+    cy.findByTestId(testid).findByTitle('More Actions').click();
+    cy.findByText('Delete').click();
+    cy.findByText('Proceed').click();
+    cy.findByText('Account deletion in progress.').should('be.visible');
+    cy.findByText('Error message from API').should('be.visible');
+  });
+
+  it('should dismiss delete account dialog', () => {
+    const testid = 'account-1';
+    cy.findByTestId(testid).findByTitle('More Actions').click();
+    cy.findByText('Delete').click();
+    cy.findByText('Cancel').click();
+    cy.findByTestId(testid).should('be.visible');
+  });
 });
