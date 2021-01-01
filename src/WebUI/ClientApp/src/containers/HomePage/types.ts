b838a441420d7261e7095840f0ae4cb7e853029b
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-01-01 14:06:56 +0800

    Rearrange types

--- src/WebUI/ClientApp/src/containers/HomePage/types.ts
+++ src/WebUI/ClientApp/src/containers/HomePage/types.ts
@@ -1,30 +1,5 @@
 import { TransactionCategory } from '../CategoryPage/types';
 
-export type THomePageContext = {
-  addAccount: (account: Account) => void;
-  setActiveAccount: (account: Account | null) => void;
-  accounts: Account[];
-  activeAccount: Account | null;
-  updateAccountBalance: (id: number, balance: number) => void;
-  deleteAccount: (id: number) => void;
-  editAccount: (id: number, account: Account) => void;
-  currentUser: User | null;
-};
-
-export interface FormDialogProps {
-  open: boolean;
-  onClose: () => void;
-  addAccount: (account: Account) => void;
-}
-
-export type Account = {
-  id: number;
-  name: string;
-  abbreviation: string;
-  balance: number;
-  transactions: Transaction[];
-};
-
 // Workaround while https://github.com/eslint/typescript-eslint-parser/issues/437 is open
 // eslint-disable-next-line no-shadow
 export enum TransactionType {
@@ -42,16 +17,41 @@ export type Transaction = {
   type: TransactionType;
 };
 
-export type UseHomePageState = {
-  setAccounts: (accounts: Account[]) => void;
-  setCurrentUser: (user: User) => void;
-} & THomePageContext;
+export type Account = {
+  id: number;
+  name: string;
+  abbreviation: string;
+  balance: number;
+  transactions: Transaction[];
+};
 
 export interface User {
   id: string;
   fullName: string;
   email: string;
 }
+
+export type THomePageContext = {
+  addAccount: (account: Account) => void;
+  setActiveAccount: (account: Account | null) => void;
+  accounts: Account[];
+  activeAccount: Account | null;
+  updateAccountBalance: (id: number, balance: number) => void;
+  deleteAccount: (id: number) => void;
+  editAccount: (id: number, account: Account) => void;
+  currentUser: User | null;
+};
+
+export interface FormDialogProps {
+  open: boolean;
+  onClose: () => void;
+  addAccount: (account: Account) => void;
+}
+
+export type UseHomePageState = {
+  setAccounts: (accounts: Account[]) => void;
+  setCurrentUser: (user: User) => void;
+} & THomePageContext;
 
 export interface NewUserTransactionCommandVm {
   transactions: Transaction[];
