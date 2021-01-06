f1ae2d23458c8edb23be3e27ed02f61dc058e874
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-01-07 00:03:54 +0800

    Add viewports

--- src/WebUI/ClientApp/cypress/integration/RegisterPage/register.spec.ts
+++ src/WebUI/ClientApp/cypress/integration/RegisterPage/register.spec.ts
@@ -1,54 +1,61 @@
 describe('RegisterPage', () => {
-  beforeEach(() => {
-    cy.visit('/register');
-  });
+  const sizes = ['iphone-4', 'ipad-mini', 'macbook-13'];
 
-  it('should display unmatch password and confirm password', () => {
-    cy.findByPlaceholderText('Password').type('abcdE123');
-    cy.findByPlaceholderText('Confirm Password')
-      .type('abcdE1234')
-      .blur();
+  sizes.forEach((size) => {
+    describe(`viewport is [${size}]`, () => {
+      beforeEach(() => {
+        cy.visit('/register');
+        cy.viewport(size);
+      });
 
-    cy.findByText('Confirm password is not the same as password.').should('exist');
-  });
+      it('should display unmatch password and confirm password', () => {
+        cy.findByPlaceholderText('Password').type('abcdE123');
+        cy.findByPlaceholderText('Confirm Password').type('abcdE1234').blur();
 
-  it('should navigate to login page', () => {
-    cy.findByText('Login').click();
+        cy.findByText('Confirm password is not the same as password.').should(
+          'exist',
+        );
+      });
 
-    cy.title().should('contain', 'Login');
-  });
+      it('should navigate to login page', () => {
+        cy.findByText('Login').click();
 
-  it('should able to create account successfully', () => {
-    cy.server();
-    cy.route('post', '/api/users', {});
-    cy.route('post', '/connect/token', {});
+        cy.title().should('contain', 'Login');
+      });
 
-    cy.findByPlaceholderText('Full Name').type('Juan');
-    cy.findByPlaceholderText('Email Address').type('juan@mailinator.com');
-    cy.findByPlaceholderText('Username').type('juan');
-    cy.findByPlaceholderText('Password').type('abcdE123');
-    cy.findByPlaceholderText('Confirm Password').type('abcdE123');
-    cy.findByText('Register').click();
+      it('should able to create account successfully', () => {
+        cy.server();
+        cy.route('post', '/api/users', {});
+        cy.route('post', '/connect/token', {});
 
-    cy.title().should('contain', 'Dashboard');
-  });
+        cy.findByPlaceholderText('Full Name').type('Juan');
+        cy.findByPlaceholderText('Email Address').type('juan@mailinator.com');
+        cy.findByPlaceholderText('Username').type('juan');
+        cy.findByPlaceholderText('Password').type('abcdE123');
+        cy.findByPlaceholderText('Confirm Password').type('abcdE123');
+        cy.findByText('Register').click();
 
-  it('should display error message when account creation is failed', () => {
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: '/api/users',
-      status: 400,
-      response: { message: 'This is a sample error message from API.' },
+        cy.title().should('contain', 'Dashboard');
+      });
+
+      it('should display error message when account creation is failed', () => {
+        cy.server();
+        cy.route({
+          method: 'POST',
+          url: '/api/users',
+          status: 400,
+          response: { message: 'This is a sample error message from API.' },
+        });
+
+        cy.findByPlaceholderText('Full Name').type('Juan');
+        cy.findByPlaceholderText('Email Address').type('juan@mailinator.com');
+        cy.findByPlaceholderText('Username').type('juan');
+        cy.findByPlaceholderText('Password').type('abcdE123');
+        cy.findByPlaceholderText('Confirm Password').type('abcdE123');
+        cy.findByText('Register').click();
+
+        cy.findByText('This is a sample error message from API.');
+      });
     });
-
-    cy.findByPlaceholderText('Full Name').type('Juan');
-    cy.findByPlaceholderText('Email Address').type('juan@mailinator.com');
-    cy.findByPlaceholderText('Username').type('juan');
-    cy.findByPlaceholderText('Password').type('abcdE123');
-    cy.findByPlaceholderText('Confirm Password').type('abcdE123');
-    cy.findByText('Register').click();
-
-    cy.findByText('This is a sample error message from API.');
   });
 });
