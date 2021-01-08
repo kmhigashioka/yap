6d689f874d00c43cfcc2cff5c5bc7aff1802cd2c
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-01-09 03:39:43 +0800

    [Feature] Category page mobile-friendly (#356)
    * [Feature] Make Category page mobile-friendly
    
    * Revert presentation on search container
    
    * Create common test
    
    * Use runOnSizes

--- src/WebUI/ClientApp/cypress/integration/CategoryPage/category-page.spec.ts
+++ src/WebUI/ClientApp/cypress/integration/CategoryPage/category-page.spec.ts
@@ -1,13 +1,17 @@
-describe('Category Page', () => {
-  beforeEach(() => {
-    cy.server();
-    cy.route('/api/TransactionCategories', 'fixture:usercategories.json');
-    cy.login();
-    cy.visit('/category');
-  });
+import { runOnSizes } from '../../common';
 
-  it('should filter Category by W', () => {
-    cy.findByPlaceholderText('Search for anything').type('W');
-    cy.findAllByTestId(/category-row-name/).should('have.length', 2);
+describe('Category Page', () => {
+  runOnSizes(() => {
+    beforeEach(() => {
+      cy.server();
+      cy.route('/api/TransactionCategories', 'fixture:usercategories.json');
+      cy.login();
+      cy.visit('/category');
+    });
+
+    it('should filter Category by W', () => {
+      cy.findByPlaceholderText('Search for anything').type('W');
+      cy.findAllByTestId(/category-row-name/).should('have.length', 2);
+    });
   });
 });
--- src/WebUI/ClientApp/cypress/integration/LoginPage/login.spec.ts
+++ src/WebUI/ClientApp/cypress/integration/LoginPage/login.spec.ts
@@ -1,47 +1,41 @@
+import { runOnSizes } from '../../common';
+
 describe('LoginPage', () => {
-  const sizes = ['iphone-4', 'ipad-mini', 'macbook-13'];
-
-  sizes.forEach((size) => {
-    describe(`viewport is [${size}]`, () => {
-      beforeEach(() => {
-        cy.viewport(size);
+  runOnSizes(() => {
+    it('should successfully login', () => {
+      cy.server();
+      cy.route('POST', '/connect/token', {
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        access_token: 'access_token',
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        refresh_token: 'refresh_token',
       });
+      cy.visit('/login');
 
-      it('should successfully login', () => {
-        cy.server();
-        cy.route('POST', '/connect/token', {
-          // eslint-disable-next-line @typescript-eslint/camelcase
-          access_token: 'access_token',
-          // eslint-disable-next-line @typescript-eslint/camelcase
-          refresh_token: 'refresh_token',
-        });
-        cy.visit('/login');
+      cy.findByPlaceholderText('Username').type('m2m');
+      cy.findByPlaceholderText('Password').type('secret');
+      cy.findByText('Login').click();
 
-        cy.findByPlaceholderText('Username').type('m2m');
-        cy.findByPlaceholderText('Password').type('secret');
-        cy.findByText('Login').click();
+      cy.title().should('contain', 'Dashboard');
+    });
 
-        cy.title().should('contain', 'Dashboard');
+    it('should display error message when using invalid credential', () => {
+      cy.server();
+      cy.route({
+        url: '/connect/token',
+        method: 'POST',
+        status: 400,
+        response: {},
       });
+      cy.visit('/login');
 
-      it('should display error message when using invalid credential', () => {
-        cy.server();
-        cy.route({
-          url: '/connect/token',
-          method: 'POST',
-          status: 400,
-          response: {},
-        });
-        cy.visit('/login');
+      cy.findByPlaceholderText('Username').type('invalid');
+      cy.findByPlaceholderText('Password').type('credential');
+      cy.findByText('Login').click();
 
-        cy.findByPlaceholderText('Username').type('invalid');
-        cy.findByPlaceholderText('Password').type('credential');
-        cy.findByText('Login').click();
-
-        cy.findByText('Invalid username or password.').should('be.visible');
-        cy.findByPlaceholderText('Username').click();
-        cy.findByText('Invalid username or password.').should('not.exist');
-      });
+      cy.findByText('Invalid username or password.').should('be.visible');
+      cy.findByPlaceholderText('Username').click();
+      cy.findByText('Invalid username or password.').should('not.exist');
     });
   });
 });
