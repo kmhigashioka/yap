948ed41a4647bb8cb717064dea735ba336e36793
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-02-04 15:52:04 +0800

    Delete context

--- src/WebUI/ClientApp/src/containers/RegisterPage/RegisterPage.tsx
+++ src/WebUI/ClientApp/src/containers/RegisterPage/RegisterPage.tsx
@@ -10,7 +10,6 @@ import {
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { useFormState } from 'react-use-form-state';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import RegisterPageContext from './RegisterPageContext';
 import Welcome from '../LoginPage/Welcome';
 import request, { TokenResponse } from '../../utils/request';
 import auth from '../../utils/auth';
@@ -114,98 +113,96 @@ const RegisterPage: React.FC<RouteComponentProps> = ({
         xsToMdDeviceMatches ? classes.loginWrapperXsToMd : ''
       }`}
     >
-      <RegisterPageContext.Provider value={{}}>
-        <Helmet>
-          <title>Register</title>
-          <meta name="description" content="" />
-        </Helmet>
-        <Welcome xsToMdDeviceMatches={xsToMdDeviceMatches} />
-        <div
-          className={`${classes.formContainer} ${
-            xsToMdDeviceMatches ? classes.formContainerXsToMd : ''
-          }`}
-        >
-          <Typography variant="h6">CREATE AN ACCOUNT</Typography>
-          <form className={classes.form} onSubmit={handleSubmit}>
-            <TextField
-              label="Full Name"
-              margin="dense"
-              variant="outlined"
-              fullWidth
-              required
-              placeholder="Full Name"
-              {...text('fullName')}
-            />
-            <TextField
-              label="Email Address"
-              margin="dense"
-              variant="outlined"
-              fullWidth
-              required
-              placeholder="Email Address"
-              {...text('email')}
-            />
-            <TextField
-              label="Username"
-              margin="dense"
-              variant="outlined"
-              fullWidth
-              required
-              placeholder="Username"
-              {...text('userName')}
-            />
-            <TextField
-              label="Password"
-              margin="dense"
-              variant="outlined"
-              fullWidth
-              required
-              placeholder="Password"
-              {...password('password')}
-            />
-            <TextField
-              label="Confirm Password"
-              margin="dense"
-              variant="outlined"
-              fullWidth
-              required
-              placeholder="Confirm Password"
-              helperText={errors.confirmPassword}
-              error={!!errors.confirmPassword}
-              {...password({
-                name: 'confirmPassword',
-                validate: (value, formvalues) => {
-                  if (value !== formvalues.password) {
-                    return 'Confirm password is not the same as password.';
-                  }
-                  return null;
-                },
-                validateOnBlur: true,
-              })}
-            />
-            <Button
-              className={classes.submitButton}
-              color="primary"
-              variant="contained"
-              type="submit"
-              fullWidth
-              disabled={isLoading}
-            >
-              Register
-            </Button>
-          </form>
-          <Typography variant="body1">Already have an account?</Typography>
-          <Link className={classes.link} to="/login">
-            <Typography variant="body2">Login</Typography>
-          </Link>
-        </div>
-        <Snackbar
-          autoHideDuration={6000}
-          open={snackbarMessage !== ''}
-          message={snackbarMessage}
-          onClose={handleCloseSnackbar}
-        />
-      </RegisterPageContext.Provider>
+      <Helmet>
+        <title>Register</title>
+        <meta name="description" content="" />
+      </Helmet>
+      <Welcome xsToMdDeviceMatches={xsToMdDeviceMatches} />
+      <div
+        className={`${classes.formContainer} ${
+          xsToMdDeviceMatches ? classes.formContainerXsToMd : ''
+        }`}
+      >
+        <Typography variant="h6">CREATE AN ACCOUNT</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <TextField
+            label="Full Name"
+            margin="dense"
+            variant="outlined"
+            fullWidth
+            required
+            placeholder="Full Name"
+            {...text('fullName')}
+          />
+          <TextField
+            label="Email Address"
+            margin="dense"
+            variant="outlined"
+            fullWidth
+            required
+            placeholder="Email Address"
+            {...text('email')}
+          />
+          <TextField
+            label="Username"
+            margin="dense"
+            variant="outlined"
+            fullWidth
+            required
+            placeholder="Username"
+            {...text('userName')}
+          />
+          <TextField
+            label="Password"
+            margin="dense"
+            variant="outlined"
+            fullWidth
+            required
+            placeholder="Password"
+            {...password('password')}
+          />
+          <TextField
+            label="Confirm Password"
+            margin="dense"
+            variant="outlined"
+            fullWidth
+            required
+            placeholder="Confirm Password"
+            helperText={errors.confirmPassword}
+            error={!!errors.confirmPassword}
+            {...password({
+              name: 'confirmPassword',
+              validate: (value, formvalues) => {
+                if (value !== formvalues.password) {
+                  return 'Confirm password is not the same as password.';
+                }
+                return null;
+              },
+              validateOnBlur: true,
+            })}
+          />
+          <Button
+            className={classes.submitButton}
+            color="primary"
+            variant="contained"
+            type="submit"
+            fullWidth
+            disabled={isLoading}
+          >
+            Register
+          </Button>
+        </form>
+        <Typography variant="body1">Already have an account?</Typography>
+        <Link className={classes.link} to="/login">
+          <Typography variant="body2">Login</Typography>
+        </Link>
+      </div>
+      <Snackbar
+        autoHideDuration={6000}
+        open={snackbarMessage !== ''}
+        message={snackbarMessage}
+        onClose={handleCloseSnackbar}
+      />
     </div>
   );
 };
