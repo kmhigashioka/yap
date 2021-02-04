5dc568c80fb15f46797e98b4c85376a418f8066f
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-02-04 16:00:00 +0800

    Refactor context

--- src/WebUI/ClientApp/src/containers/Language/index.ts
+++ src/WebUI/ClientApp/src/containers/Language/index.ts
@@ -1,6 +1,4 @@
 import Language from './Language';
-import Context from './LanguageContext';
 
 export { useIntl } from 'react-intl';
 export default Language;
-export const LanguageContext = Context;
