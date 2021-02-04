4e98edeef92158fb258e78269b85c5c5758f785e
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-02-04 16:13:41 +0800

    Refactor context

--- src/WebUI/ClientApp/src/containers/HomePage/HomePageContext.tsx
+++ src/WebUI/ClientApp/src/containers/HomePage/HomePageContext.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { THomePageContext } from './types';
+import { THomePageContext, Account, User } from './types';
 
-const HomePageContext = React.createContext<THomePageContext | undefined>(
-  undefined,
-);
+export const HomePageContext = React.createContext<
+  THomePageContext | undefined
+>(undefined);
 
 HomePageContext.displayName = 'HomePageContext';
 
-export const useHomePageContext = (): THomePageContext => {
+export function useHomePageContext(): THomePageContext {
   const context = React.useContext(HomePageContext);
   if (context === undefined) {
     throw new Error(
@@ -15,6 +15,94 @@ export const useHomePageContext = (): THomePageContext => {
     );
   }
   return context;
-};
+}
 
-export default HomePageContext;
+export function HomePageProvider({
+  accounts,
+  children,
+  currentUser,
+  setAccounts,
+  setCurrentUser,
+}: {
+  accounts: Account[];
+  children: React.ReactNode;
+  currentUser: User | null;
+  setAccounts: (accounts: Account[]) => void;
+  setCurrentUser: (user: User | null) => void;
+}): React.ReactElement {
+  const [activeAccount, setActiveAccount] = React.useState<Account | null>(
+    null,
+  );
+
+  const addAccount = (account: Account): void => {
+    setAccounts([...accounts, account]);
+  };
+
+  const updateAccountBalance = (id: number, balance: number): void => {
+    const newAccounts = accounts.map((a) => {
+      const newAccount = { ...a };
+      if (newAccount.id === id) {
+        newAccount.balance = balance;
+      }
+      return newAccount;
+    });
+
+    setAccounts(newAccounts);
+  };
+
+  const deleteAccount = (id: number): void => {
+    const newAccounts = accounts.filter((a) => a.id !== id);
+    setAccounts(newAccounts);
+  };
+
+  const editAccount = (id: number, account: Account): void => {
+    const newAccounts = accounts.map((a) => {
+      let newAccount = { ...a };
+
+      if (id === a.id) {
+        newAccount = {
+          ...newAccount,
+          balance: account.balance,
+          abbreviation: account.abbreviation,
+          name: account.name,
+        };
+      }
+
+      return newAccount;
+    });
+
+    setAccounts(newAccounts);
+  };
+
+  React.useEffect(() => {
+    if (activeAccount === null) {
+      return;
+    }
+    const newAccount = accounts.find(
+      (account) => account.id === activeAccount.id,
+    );
+    if (!newAccount) {
+      setActiveAccount(null);
+      return;
+    }
+    setActiveAccount(newAccount);
+  }, [accounts, activeAccount]);
+
+  const value = {
+    addAccount,
+    setActiveAccount,
+    accounts,
+    activeAccount,
+    updateAccountBalance,
+    deleteAccount,
+    editAccount,
+    currentUser,
+    setCurrentUser,
+  };
+
+  return (
+    <HomePageContext.Provider value={value}>
+      {children}
+    </HomePageContext.Provider>
+  );
+}
