75ac4a9450f2d0c32f27595e50cdd898c636c7ec
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-02-21 19:06:58 +0800

    Refactor test

--- src/WebUI/ClientApp/cypress/integration/RegisterPage/register.spec.ts
+++ src/WebUI/ClientApp/cypress/integration/RegisterPage/register.spec.ts
@@ -1,49 +1,41 @@
 import { runOnSizes } from '../../common';
 
+const pageRoute = '/register';
+
 describe('RegisterPage', () => {
   runOnSizes(() => {
-    beforeEach(() => {
-      cy.visit('/register');
-    });
-
-    it('should display unmatch password and confirm password', () => {
-      cy.findByPlaceholderText('Password').type('abcdE123');
-      cy.findByPlaceholderText('Confirm Password').type('abcdE1234').blur();
-
-      cy.findByText('Confirm password is not the same as password.').should(
-        'exist',
-      );
-    });
-
     it('should navigate to login page', () => {
+      cy.visit(pageRoute);
       cy.findByText('Login').click();
 
       cy.title().should('contain', 'Login');
     });
 
     it('should able to create account successfully', () => {
-      cy.server();
-      cy.route('post', '/api/users', {});
-      cy.route('post', '/connect/token', {});
+      cy.intercept('POST', '/api/users', { body: {} });
+      cy.intercept('POST', '/connect/token', { body: {} });
+      cy.visit(pageRoute);
 
       cy.findByPlaceholderText('Full Name').type('Juan');
       cy.findByPlaceholderText('Email Address').type('juan@mailinator.com');
       cy.findByPlaceholderText('Username').type('juan');
       cy.findByPlaceholderText('Password').type('abcdE123');
-      cy.findByPlaceholderText('Confirm Password').type('abcdE123');
+      cy.findByPlaceholderText('Confirm Password').type('abcdE1234').blur();
+      cy.findByText('Confirm password is not the same as password.').should(
+        'exist',
+      );
+      cy.findByPlaceholderText('Confirm Password').clear().type('abcdE1234');
       cy.findByText('Register').click();
 
-      cy.title().should('contain', 'Dashboard');
+      cy.findByText(/welcome/i, { timeout: 60000 }).should('be.visible');
     });
 
     it('should display error message when account creation is failed', () => {
-      cy.server();
-      cy.route({
-        method: 'POST',
-        url: '/api/users',
-        status: 400,
-        response: { message: 'This is a sample error message from API.' },
+      cy.intercept('POST', '/api/users', {
+        statusCode: 400,
+        body: { message: 'This is a sample error message from API.' },
       });
+      cy.visit(pageRoute);
 
       cy.findByPlaceholderText('Full Name').type('Juan');
       cy.findByPlaceholderText('Email Address').type('juan@mailinator.com');
