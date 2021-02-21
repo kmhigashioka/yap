1fe5ed55b3713b17a42f71a3d1ce5685f6881e4c
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-02-21 20:05:40 +0800

    Remove unused codes

--- src/WebUI/ClientApp/cypress/support/commands.js
+++ src/WebUI/ClientApp/cypress/support/commands.js
@@ -1,9 +1,1 @@
 import '@testing-library/cypress/add-commands';
-
-// Cypress.on('window:before:load', win => {
-//   win.fetch = null;
-// });
-
-Cypress.Commands.add('login', () => {
-  cy.route('POST', '/connect/token', {});
-});
